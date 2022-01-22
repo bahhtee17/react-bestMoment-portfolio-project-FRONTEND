a89c9c5a961bb62e5d7a988554d0ce744e5c3c23
Baxtiyor <baxtiyorshukurjanov36@gmail.com>
2022-01-22 23:27:09 +0500

    jsx structure is built

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {Container, AppBar, Typography, Grid, Grow} from "@material-ui/core";
+import Form from "./components/Form/Form";
+import Posts from "./components/Posts/Posts";
+import memories from "./images/memories.png";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth='lg'>
+      <AppBar position='static' color='inherit'>
+        <Typography variant='h2' align='center'>
+          Best Moments
+        </Typography>
+        <img src={memories} alt='moments' height='150' />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justifyContent='space-between'
+            alignItems='stretch'
+            spacing='3'>
+            <Grid xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 }
 
