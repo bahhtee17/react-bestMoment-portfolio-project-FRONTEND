af01a37f500053d14fd9b4e1359212838f4b96c5
Baxtiyor <baxtiyorshukurjanov36@gmail.com>
2022-01-23 15:08:20 +0500

    redux is added to project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import {Container, AppBar, Typography, Grid, Grow} from "@material-ui/core";
 import Form from "./components/Form/Form";
+import React, {useEffect} from "react";
 import Posts from "./components/Posts/Posts";
 import memories from "./images/memories.png";
 import useStyles from "./styled";
+import {useDispatch} from "react-redux";
+import {getPosts} from "./actions/posts";
+
 function App() {
   const classes = useStyles();
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getPosts());
+  }, []);
   return (
     <Container maxWidth='lg'>
       <AppBar className={classes.appBar} position='static' color='inherit'>
--- src/components/Posts/Post/Post.js
+++ src/components/Posts/Post/Post.js
@@ -1,7 +1,7 @@
 import React from "react";
-import useStyles from "./style";
+import useStyles from "./styles";
 const Post = () => {
-  const classes = useStyles();
+  //const classes = useStyles();
   return <div>Post</div>;
 };
 
