1346375fc4a440b68dd4d71abfaa66c53ac2c47c
Baxtiyor <baxtiyorshukurjanov36@gmail.com>
2022-01-24 21:10:50 +0500

    form structure is added

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,8 +1,93 @@
-import React from "react";
-//import useStyles from "./styles";
+import {TextField, Button, Typography, Paper} from "@material-ui/core";
+import React, {useState} from "react";
+import FileBase from "react-file-base64";
+import useStyles from "./styles";
 const Form = () => {
-  //const classes = useStyles();
-  return <div>Form</div>;
+  const classes = useStyles();
+  const [postsData, setPostsData] = useState({
+    creator: "",
+    title: "",
+    message: "",
+    tags: "",
+    selectedFile: "",
+  });
+
+  const submitHandler = (event) => {};
+  const clear = (event) => {};
+  return (
+    <Paper className={classes.paper}>
+      <form
+        autoComplete='off'
+        noValidate
+        className={`${classes.root} ${classes.form}`}
+        onSubmit={submitHandler}>
+        <Typography variant='h6'>Creating a Moment</Typography>
+        <TextField
+          name='creator'
+          variant='outlined'
+          label='Creator'
+          fullWidth
+          onChange={(e) =>
+            setPostsData({...postsData, creator: e.target.value})
+          }
+          value={postsData.creator}
+        />
+        <TextField
+          name='title'
+          variant='outlined'
+          label='Title'
+          fullWidth
+          onChange={(e) => setPostsData({...postsData, title: e.target.value})}
+          value={postsData.title}
+        />
+        <TextField
+          name='message'
+          variant='outlined'
+          label='Message'
+          fullWidth
+          onChange={(e) =>
+            setPostsData({...postsData, message: e.target.value})
+          }
+          value={postsData.message}
+        />
+        <TextField
+          name='tags'
+          variant='outlined'
+          label='Tags'
+          fullWidth
+          onChange={(e) => setPostsData({...postsData, tags: e.target.value})}
+          value={postsData.tags}
+        />
+        <div className={classes.fileInput}>
+          <FileBase
+            type='file'
+            miltiple={false}
+            onDone={({base64}) =>
+              setPostsData({...postsData, selectedFile: base64})
+            }
+          />
+        </div>
+        <Button
+          className={classes.buttonSubmit}
+          variant='container'
+          color='primary'
+          size='large'
+          type='submit'
+          fullWidth
+          onClick={submitHandler}>
+          Submit
+        </Button>
+        <Button
+          variant='contained'
+          color='secondary'
+          size='small'
+          onClick={clear}
+          fullWidth>
+          Clear
+        </Button>
+      </form>
+    </Paper>
+  );
 };
 
 export default Form;
--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,10 +1,10 @@
 import React from "react";
 import Post from "./Post/Post";
-//import useStyles from "./styles";
+import useStyles from "./styles";
 import {useSelector} from "react-redux";
 const Posts = () => {
   const posts = useSelector((state) => state.posts);
-  //const classes = useStyles();
+  const classes = useStyles();
   console.log(posts);
   return (
     <>
